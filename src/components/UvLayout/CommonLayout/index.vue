2645d76bbc2f92ac36fba0043b362ef37b2cd665
leiax00 <leiax00@outlook.com>
2021-10-29 15:55:28 +0800

    优化项目结构

---
+++ src/components/UvLayout/CommonLayout/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="common-layout">
+    common layout
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default { name: 'CommonLayout' }
+</script>
+
+<style scoped>
+
+</style>
